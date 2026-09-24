3f80053def28e24ca00d12de5dff4625dd0a1c07
Sxmmie <samuelumohjnr@gmail.com>
2026-09-24 11:18:50 +0100

    Added the create and list methods for the Todo Repo

--- src/repositories/todo_repo.rs
+++ src/repositories/todo_repo.rs
@@ -1,4 +1,10 @@
+use anyhow::Ok;
 use sqlx::PgPool;
+
+use crate::{
+	error::AppResult,
+	models::{todo::Todo, user},
+};
 
 pub struct TodoRepository<'a> {
 	pool: &'a PgPool,
@@ -7,5 +13,29 @@ pub struct TodoRepository<'a> {
 impl<'a> TodoRepository<'a> {
 	pub fn new(pool: &'a PgPool) -> Self {
 		Self { pool }
+	}
+
+	pub async fn create(&self, user_id: i64, title: &str, description: &str) -> AppResult<Todo> {
+		let todo = sqlx::query_as::<_, Todo>(
+			r#"INSERT INTO todos (user_id, title, description, completed)
+					VALUES ($1, $2, $3, false)
+					RETURNING id, user_id, title, description, completed, created_at, updated_at"#,
+		)
+		.bind(user_id)
+		.bind(title)
+		.bind(description)
+		.fetch_one(self.pool)
+		.await?;
+
+		Ok(todo)
+	}
+
+	pub async fn list_for_user(&self, user_id: i64) -> AppResult<Vec<Todo>> {
+		let todos = sqlx::query_as::<_, Todo>("SELECT * FROM todos WHERE user_id = $1 ORDER BY created_at DESC")
+			.bind(user_id)
+			.fetch_all(self.pool)
+			.await?;
+
+		Ok(todos)
 	}
 }
